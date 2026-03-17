7f25304723f75b93e7fdf6a44241ab2a6249db2e
Alastair Wyse <alastair@alastairwyse.net>
2026-03-17 22:24:46 +1100

    Created shell of first class

---
+++ PowerGrid.Core/GridComparer.cs
@@ -0,0 +1,28 @@
+﻿/*
+* Copyright 2026 Alastair Wyse (https://github.com/alastairwyse/PowerGrid/)
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* 
+*     http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+namespace PowerGrid.Core
+{
+    /// <summary>
+    /// Analyzes and outputs the differences between the sorted contents of two grids.
+    /// </summary>
+    /// <typeparam name="T">The type of data stored in the grid.</typeparam>
+    public class GridComparer<T>
+    {
+        // 2x Enumerable input of type T
+        // Need to take 3x 'emitters' on constructor or method to emit added, updated, and deleted data points
+    }
+}
