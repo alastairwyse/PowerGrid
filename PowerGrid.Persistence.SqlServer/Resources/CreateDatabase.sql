f0a8feaef11cc077f39e360702280122e4230572
Alastair Wyse <alastair@alastairwyse.net>
2026-08-28 21:48:33 +1000

    WIP commit

--- PowerGrid.Persistence.SqlServer/Resources/CreateDatabase.sql
+++ PowerGrid.Persistence.SqlServer/Resources/CreateDatabase.sql
@@ -47,3 +47,30 @@ CREATE TABLE $(DatabaseName).dbo.StockPriceGrids
 );
 
 CREATE INDEX StockPriceGridsOuterKeysIndex ON $(DatabaseName).dbo.StockPriceGrids (Tag, DataSource, [Date], [Version]);
+
+CREATE TABLE $(DatabaseName).dbo.WeatherForecasts
+(
+    Id               bigint        IDENTITY(1,1) PRIMARY KEY NOT NULL, 
+    Tag              nvarchar(50)  NOT NULL, 
+    [Date]           date          NOT NULL, 
+    Country          nvarchar(50)  NOT NULL, 
+    City             nvarchar(50)  NOT NULL, 
+    [Time]           time          NOT NULL, 
+    Temperature      int           NOT NULL, 
+    TransactionFrom  datetime2     NOT NULL, 
+    TransactionTo    datetime2     NOT NULL
+);
+
+CREATE INDEX WeatherForecastsOuterKeysIndex ON $(DatabaseName).dbo.WeatherForecasts (Tag, [Date], TransactionTo);
+CREATE INDEX WeatherForecastsTransactionIndex ON $(DatabaseName).dbo.WeatherForecasts (TransactionTo, TransactionFrom);
+
+CREATE TABLE $(DatabaseName).dbo.WeatherForecastGrids
+(
+    Id                    bigint        IDENTITY(1,1) PRIMARY KEY  NOT NULL, 
+    Tag                   nvarchar(50)  NOT NULL, 
+    [Date]                date          NOT NULL, 
+    [Version]             int           NOT NULL, 
+    TransactionTimestamp  datetime2     NOT NULL
+);
+
+CREATE INDEX WeatherForecastGridsOuterKeysIndex ON $(DatabaseName).dbo.WeatherForecastGrids (Tag, [Date], [Version]);
